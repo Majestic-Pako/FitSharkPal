4e7a014fc628b8a5166c637a1df0ecb4e862e344
tomy2311 <87246337+EstebanRacana@users.noreply.github.com>
2025-04-07 17:16:20 -0300

    Update xd
    hola

--- src/Code/Main.java
+++ src/Code/Main.java
@@ -1,10 +1,12 @@
 package Code;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+       JOptionPane.showMessageDialog(null, "hola soy esteban");
 	}
 
 }
