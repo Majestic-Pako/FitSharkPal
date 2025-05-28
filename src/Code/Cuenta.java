aea5ad89328e34f567faa62e7887d4adbdf12c61
Esteban <87246337+EstebanRacana@users.noreply.github.com>
2025-05-28 02:54:27 +0200

    Update Cuenta.java

--- src/Code/Cuenta.java
+++ src/Code/Cuenta.java
@@ -1,22 +1,33 @@
 package Code;
 
-public class Cuenta {
-	
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.mysql.jdbc.Statement;
+
+public class Cuenta<T extends Cuenta> implements Encriptador {
+	private int idCuenta;
 	private String nombre;
 	private String usuario;
-	private String contrasenia;
+	private String contrasena;
 	private int edad;
 	private String genero;
-	private Boolean Entrenador;
-	
-	public Cuenta(String nombre, String usuario, String contrasenia, int edad, String genero, Boolean entrenador) {
+	private Rol rol;
+
+	public Cuenta(int idCuenta, String nombre, String usuario, String contrasena, int edad, String genero,
+			Rol rol) {
 		super();
+		this.idCuenta = idCuenta;
 		this.nombre = nombre;
 		this.usuario = usuario;
-		this.contrasenia = contrasenia;
+		this.contrasena = contrasena;
 		this.edad = edad;
 		this.genero = genero;
-		Entrenador = entrenador;
+		this.rol = rol;
+	}
+
+	public Cuenta() {
 	}
 
 	public String getNombre() {
@@ -35,12 +46,12 @@ public class Cuenta {
 		this.usuario = usuario;
 	}
 
-	public String getContrasenia() {
-		return contrasenia;
+	public String getContrasena() {
+		return contrasena;
 	}
 
-	public void setContrasenia(String contrasenia) {
-		this.contrasenia = contrasenia;
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
 	}
 
 	public int getEdad() {
@@ -59,13 +70,87 @@ public class Cuenta {
 		this.genero = genero;
 	}
 
-	public Boolean getEntrenador() {
-		return Entrenador;
+	public int getIdCuenta() {
+		return idCuenta;
 	}
 
-	public void setEntrenador(Boolean entrenador) {
-		Entrenador = entrenador;
+	public void setIdCuenta(int id) {
+		this.idCuenta = idCuenta;
 	}
 	
+
+	public Rol getRol() {
+		return rol;
+	}
+
+	public void setRol(Rol rol) {
+		this.rol = rol;
+	}
+
+
+
+	private static Connection con = Conexion.getInstance().getConnection();
 	
+	public static int Registro(String usuario, String contrasena, String rol) {
+		try {
+	        String sql = "INSERT INTO cuenta (usuario, contrasena, rol) VALUES (?, ?, ?)";
+	        PreparedStatement stmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+	        Encriptador cifrador = new Cuenta(); //no me dejaba encriptar si no ponia esto :v
+	        stmt.setString(1, usuario);
+	        stmt.setString(2, cifrador.encriptar(contrasena));
+	        stmt.setString(3, rol);
+	        int filas = stmt.executeUpdate();
+	        if (filas > 0) {
+	            ResultSet rs = stmt.getGeneratedKeys();
+	            if (rs.next()) {
+	                return rs.getInt(1); //esta wea es para que me devuelva el idCuenta pa el Cliente
+	            }
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+		return -1;
+	}
+
+	 public static Cuenta<?> Login(String usuario, String contrasena) {
+		 Cuenta cuenta = null;
+	        try {
+	            PreparedStatement stmt = con.prepareStatement(
+	                "SELECT * FROM cuenta WHERE usuario = ? AND contrasena = ?"
+	            );
+	            stmt.setString(1, usuario);
+	            stmt.setString(2, new Cuenta<>().encriptar(contrasena));
+	            
+	            System.out.println("Ejecutando consulta login para usuario: " + usuario);
+	            
+	            ResultSet rs = stmt.executeQuery();
+
+	            if (rs.next()) {
+	                int id = rs.getInt("idCuenta");
+	                String user = rs.getString("usuario");
+	                String pass = rs.getString("contrasena");
+	                String rolStr = rs.getString("rol");
+
+	                Rol rol = Rol.valueOf(rolStr.toUpperCase());
+	                
+	                
+	                if (rol == Rol.ENTRENADOR) {
+	                    return new Entrenador(id, null, user, pass, 0, null, rol);
+	                } else {
+	                    return new Cliente(id, null, user, pass, 0, null, rol);
+	                }
+	            }
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	        return null;
+	    }
+
+	 @Override
+	 public String toString() {
+	     return "Contraseña: " + contrasena + ", Usuario: " + usuario + ", Rol: " + rol;
+	 }
+
+
 }
+
