37a588a25886a641eebae719e9133a6ca8cb6541
Anthony2080 <antonio.pirotta@davinci.edu.ar>
2025-06-09 17:56:08 -0300

    avanzading
    ahora rutina se extiende de gamificacion y gamificacion de configuracion para darle puntaje a los ejercicios antes de dar el mensaje final de la rutina completa

--- src/Code/Gamificacion.java
+++ src/Code/Gamificacion.java
@@ -2,30 +2,23 @@ package Code;
 
 import java.util.ArrayList;
 
-public class Gamificacion {
-private int ptsFuerza;
-private int ptsResistencia;
-private int ptsTotal;
+public class Gamificacion extends ConfigEjercicios{
+	private int pts;
+	private int ptsTotal;
 private ArrayList<String>Carta= new ArrayList<String>();
 
-public Gamificacion(int ptsFuerza, int ptsResistencia, int ptsTotal, ArrayList<String> carta) {
-	super();
-	this.ptsFuerza = ptsFuerza;
-	this.ptsResistencia = ptsResistencia;
+public Gamificacion(int pts, int ptsTotal, ArrayList<String> carta, String piernas, String brazos, String pecho, String espalda, String zonaMedia,
+		String cardio, int repeticiones, int series, int cantPeso, int pausaEntreSerie, int tiempo) {
+	super(piernas, brazos, pecho, espalda, zonaMedia, cardio, repeticiones, series, cantPeso, pausaEntreSerie, tiempo);
+	this.pts = pts;
 	this.ptsTotal = ptsTotal;
 	Carta = carta;
 }
-public int getPtsFuerza() {
-	return ptsFuerza;
+public int getPts() {
+	return pts;
 }
-public void setPtsFuerza(int ptsFuerza) {
-	this.ptsFuerza = ptsFuerza;
-}
-public int getPtsResistencia() {
-	return ptsResistencia;
-}
-public void setPtsResistencia(int ptsResistencia) {
-	this.ptsResistencia = ptsResistencia;
+public void setPts(int pts) {
+	this.pts = pts;
 }
 public int getPtsTotal() {
 	return ptsTotal;
@@ -39,12 +32,25 @@ public ArrayList<String> getCarta() {
 public void setCarta(ArrayList<String> carta) {
 	Carta = carta;
 }
-@Override
-public String toString() {
-	return "Gamificacion [ptsFuerza=" + ptsFuerza + ", ptsResistencia=" + ptsResistencia + ", ptsTotal=" + ptsTotal
-			+ ", Carta=" + Carta + ", getPtsFuerza()=" + getPtsFuerza() + ", getPtsResistencia()=" + getPtsResistencia()
-			+ ", getPtsTotal()=" + getPtsTotal() + ", getCarta()=" + getCarta() + ", getClass()=" + getClass()
-			+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+
+//Ahi va el mensaje de Esteban :]
+public static boolean cartasPuntuaje(int pts, int ptsTotal,String Carta) {
+	if(pts<=0) {
+		Carta="Nada";
+		ptsTotal+=pts;
+		return true;
+		//falta un nombre para del 1 hasta el 3 xd
+	}else if(pts>=4 && pts<=6) {
+		Carta="Medium";
+		ptsTotal+=pts;
+		return true;
+	}else if(pts>=7 && pts<=10) {
+		Carta="Pro";
+		ptsTotal+=pts;
+		return true;
+	}
+	return false;
+	
 }
 
 
---
+++ src/DLL/Rutina.java
@@ -0,0 +1,37 @@
+package DLL;
+
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+import BLL.Gamificacion;
+
+public class Rutina extends Gamificacion{
+
+	public Rutina(int pts, int ptsTotal, ArrayList<String> carta, String piernas, String brazos, String pecho,
+			String espalda, String zonaMedia, String cardio, int repeticiones, int series, int cantPeso,
+			int pausaEntreSerie, int tiempo) {
+		super(pts, ptsTotal, carta, piernas, brazos, pecho, espalda, zonaMedia, cardio, repeticiones, series, cantPeso,
+				pausaEntreSerie, tiempo);
+	}
+
+	public static void Ver(int pts, int ptsTotal, ArrayList<String> carta, String piernas, String brazos, String pecho, String espalda, String zonaMedia, String cardio,
+			int repeticiones, int series, int cantPeso, int pausaEntreSerie, int tiempo) {
+		
+		JOptionPane.showMessageDialog(null, 
+				"RUTINA COMPLETA:" + 
+				"\n Entrada en calor: " + cardio + " por " + tiempo + " minutos" +
+				"\n Ejercicios: " + 
+				"\n" + zonaMedia + " -> " + repeticiones + " reps x" + series + " sets" +
+				"\n" + brazos + " -> " + repeticiones + " reps x" + series + " sets, con " + cantPeso + "kg" +
+				"\n" + pecho + " -> " + repeticiones + " reps x" + series + " sets, con " + cantPeso + "kg" +
+				"\n" + espalda + " -> " + repeticiones + " reps x" + series + " sets, con " + cantPeso + "kg" +
+				"\n" + piernas + " -> " + repeticiones + " reps x" + series + " sets, con " + cantPeso + "kg" +
+				"\n Pausa recomendada entre cada set: " + pausaEntreSerie +
+				"\n Calificacion Final = " + ptsTotal + 
+				"\n Clasificacion = " + carta);
+		//Calificacion final= Pensar nombre
+		//Clasifiacion==
+	}
+
+}
